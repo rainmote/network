fd58bf1c5d84523ea19d12fde38c460ef3cbac2a
rainmote <rainmote@gmail.com>
2017-01-08 22:27:30 +0800

    first submit

---
+++ portscanner.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import optparse
+import socket
+
+def connScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        connSkt.send('ViolentPython\r\n')
+        results = connSkt.recv(100)
+        print '[+] %d/tcp open' % tgtPort
+        print '[+] ' % str(results)
+        connSkt.close()
+    except:
+        print '[-] %d/tcp closed' % tgtPort
+
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = socket.gethostbyname(tgtHost)
+    except:
+        print "[-] Cannot resolve '%s': Unknown host" % tgtHost
+        return
+
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print '\n[+] Scan Results for: ' + tgtName[0]
+    except:
+        print '\n[+] Scan Results for: ' + tgtIP
+    socket.setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        print 'Scanning port ' + tgtPort
+        connScan(tgtHost, int(tgtPort))
+
+def main():
+    parser = optparse.OptionParser('usage%prog -H <target host> -p <target port>')
+    parser.add_option('-H', dest='tgtHost', type='string', \
+        help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', \
+        help='specify target port')
+
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(',')
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print parser.usage
+        exit(0)
+    portScan(tgtHost, tgtPorts)
+
+if __name__ == '__main__':
+    main()
+
